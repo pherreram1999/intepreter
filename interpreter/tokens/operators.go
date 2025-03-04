ac4160e09e478023aed4af9da3da3e401d85c8de
Angel <aluisl1900@aliumno.ipn.mx>
2025-03-03 19:42:52 -0600

    se agrego el tipotoken de los operadores y signos de puntuacion

--- interpreter/tokens/operators.go
+++ interpreter/tokens/operators.go
@@ -1,5 +1,15 @@
 package tokens
 
 const (
-	LeftPad TipoToken = iota
+	Minus        TipoToken = iota
+	Plus         TipoToken = iota
+	Slash        TipoToken = iota //Division
+	Star         TipoToken = iota //Multiplicacion
+	Bang         TipoToken = iota //Diferente
+	Equal        TipoToken = iota
+	EqualEqual   TipoToken = iota
+	Greater      TipoToken = iota
+	GreaterEqual TipoToken = iota
+	Less         TipoToken = iota
+	LessEqual    TipoToken = iota
 )
