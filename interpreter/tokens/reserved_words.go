b1cf9130b8ee27c91fac9810aea071362994897e
Pedro Alonso <palhm.99@gmail.com>
2025-03-03 19:07:38 -0600

    tokens

---
+++ interpreter/tokens/reserved_words.go
@@ -0,0 +1,18 @@
+package tokens
+
+type TipoToken uint
+
+const (
+	// reserved keys
+	Else   TipoToken = iota // 0
+	Fun    TipoToken = iota // 1
+	Print  TipoToken = iota
+	Var    TipoToken = iota
+	False  TipoToken = iota
+	If     TipoToken = iota
+	Return TipoToken = iota
+	While  TipoToken = iota
+	For    TipoToken = iota
+	Null   TipoToken = iota
+	True   TipoToken = iota
+)
